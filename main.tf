6d3fa63ea7aea3159bee62768b90c96908a8fa27
Ralph Pimentel <Ralph_Pimentel@ultimatesoftware.com>
2018-11-15 11:58:37 -0500

    Simplify structure

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 # Top-level folder under an organization.
 
 data "google_active_folder" "new_active_parent" {
-  display_name = "${element(var.parent_folder_name  , 0)}"
+  display_name = "${var.parent_folder_name}"
   parent = "${var.parent_id}"  
 }
 
 resource "google_folder" "parent_folder_creation" {
-  count = "${length(var.folder_name)}"
-  display_name = "${element(var.folder_name, count.index)}"
+  #count = "${length(var.folder_name)}"
+  #display_name = "${element(var.folder_name, count.index)}"
+  display_name = "${var.folder_name}"
   parent = "${data.google_active_folder.new_active_parent.name}"
   depends_on = ["data.google_active_folder.new_active_parent"]
 }
@@ -21,7 +22,8 @@ resource "google_folder" "child_folder_creation" {
   count= "${length(var.childlist)}"
   #display_name = "${element(var.children, count.index)}"
   display_name = "${lookup(var.childlist[count.index], "child")}"
-  parent = "${element(google_folder.parent_folder_creation.name, 0)}"
+  parent = "${google_folder.parent_folder_creation.name}"
+  #parent = "${element(google_folder.parent_folder_creation.name, 0)}"
   #parent = "${[element(google_folder.parent_folder_creation.*.id, count.index)}"
   
   
--- variables.tf
+++ variables.tf
@@ -3,11 +3,9 @@ variable "parent_id" {
 }
 
 variable "parent_folder_name" {
-  type = "list"
 }
 
 variable "folder_name" {
-  type = "list"
   
 }
 
