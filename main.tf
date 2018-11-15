081ccff38122b21a3af6d9f3b4597e34d5884446
Ralph Pimentel <Ralph_Pimentel@ultimatesoftware.com>
2018-11-15 11:16:13 -0500

    Moved depends on again

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "google_folder" "parent_folder_creation" {
 data "google_active_folder" "new_active_parent" {
   display_name = "${element(var.folder_name, 0)}"
   parent = "folders/${var.parent_folder_id}"
+  depends_on = ["google_folder.parent_folder_creation"]
   
 }
 
@@ -20,7 +21,7 @@ resource "google_folder" "child_folder_creation" {
   display_name = "${lookup(var.childlist[count.index], "child")}"
   parent = "${data.google_active_folder.new_active_parent.name}"
   #parent = "${[element(google_folder.parent_folder_creation.*.id, count.index)}"
-  depends_on = ["google_folder.parent_folder_creation"]
+  
   
 }
 
