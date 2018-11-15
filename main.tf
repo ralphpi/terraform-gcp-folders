c95aa9009d8d003338a7793f32521c8c374011ce
Ralph Pimentel <Ralph_Pimentel@ultimatesoftware.com>
2018-11-15 11:11:22 -0500

    move depend on to child folder creation

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ resource "google_folder" "parent_folder_creation" {
 data "google_active_folder" "new_active_parent" {
   display_name = "${element(var.folder_name, 0)}"
   parent = "folders/${var.parent_folder_id}"
-  depend_on = "google_folder.parent_folder_creation[0]"
   
 }
 
@@ -21,6 +20,7 @@ resource "google_folder" "child_folder_creation" {
   display_name = "${lookup(var.childlist[count.index], "child")}"
   parent = "${data.google_active_folder.new_active_parent.name}"
   #parent = "${element(google_folder.parent_folder_creation.*.id, count.index)}"
+  depends_on = "google_folder.parent_folder_creation[0]"
   
 }
 
