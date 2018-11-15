a272ff197bfdd56102066c3b1911002f5b0e6273
Ralph Pimentel <Ralph_Pimentel@ultimatesoftware.com>
2018-11-15 11:48:32 -0500

    correct child parent

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Top-level folder under an organization.
 
 data "google_active_folder" "new_active_parent" {
-  display_name = "${element(var.parent_folder_name, 0)}"
+  display_name = "${element(var.parent_folder_name  , 0)}"
   parent = "${var.parent_id}"  
 }
 
@@ -14,13 +14,18 @@ resource "google_folder" "parent_folder_creation" {
 
 #Second Level AKA Child  
 
+data "google_active_folder" "new_active_parent2" {
+  display_name = "${element(var.parent_folder_name  , 0)}"
+  parent = "${var.parent_id}"  
+}
+
 
 # Destroy Limitations: You must Destroy in order from right to left of childlist Array
 resource "google_folder" "child_folder_creation" {
   count= "${length(var.childlist)}"
   #display_name = "${element(var.children, count.index)}"
   display_name = "${lookup(var.childlist[count.index], "child")}"
-  parent = "${data.google_active_folder.new_active_parent.name}"
+  parent = "${element(google_folder.parent_folder_creation.name, 0)}"
   #parent = "${[element(google_folder.parent_folder_creation.*.id, count.index)}"
   
   
